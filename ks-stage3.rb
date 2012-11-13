aa74c515ca8b415a484a1617e0c1a93da92098b7
Ian Unruh <ianunruh@gmail.com>
2012-11-13 14:22:58 -0600

    WORKS

--- ks-stage3.rb
+++ ks-stage3.rb
@@ -3,7 +3,7 @@ require "mongo"
 require "pp"
 
 connection = Mongo::Connection.new("localhost", 27017)
-db = connection.db("fixture")
+db = connection.db("vm_db_fixture")
 
 interfaces = db.collection("interface")
 
@@ -19,32 +19,32 @@ client = rpcclient("provision")
 client.verbose = true
 
 client.discover.map do |node|
-		client_util.identity_filter node
-		
-		result = client_util.inventory.first
-		facts = result[:data][:facts]
-		
-		macaddr = facts["macaddress"]
+    client_util.identity_filter node
 
-		interface = interfaces.find_one(:mac_address => macaddr)
-		if interface.nil?
-				puts "No match in database for MAC address #{macaddr}"
-				next
-		end
-		instance = db.dereference(interface["instance"])
-		hostname = instance["hostname"]
-		
-		client.fact_filter "macaddress", macaddr
-		puts "Setting hostname\n"
-		printrpc client.set_hostname(:hostname => hostname)
-        printrpc client_ca.clean(:certname => hostname)
-		puts "Requesting certificate\n"
-        printrpc client.request_certificate
-        printrpc client_ca.sign(:certname => hostname)
-        puts "Starting Puppet run\n"
-        printrpc client.run_puppet
+    result = client_util.inventory.first
+    facts = result[:data][:facts]
+
+    macaddr = facts["macaddress"]
+
+    interface = interfaces.find_one(:mac_address => macaddr)
+    if interface.nil?
+        puts "No match in database for MAC address #{macaddr}"
+        next
+    end
+    instance = db.dereference(interface["instance"])
+    hostname = instance["hostname"]
+
+    client.fact_filter "macaddress", macaddr
+
+    puts "Setting hostname\n"
+    printrpc client.set_hostname(:hostname => hostname)
+    printrpc client_ca.clean(:certname => hostname)
+    puts "Requesting certificate\n"
+    printrpc client.run_puppet
+    printrpc client_ca.sign(:certname => hostname)
+    puts "Starting Puppet run\n"
+    printrpc client.run_puppet
 
 end
 
 printrpcstats
-
