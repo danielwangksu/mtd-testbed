c6ab54b7c3633d374987b4872841fc283f3391a7
Ian Unruh <ianunruh@gmail.com>
2012-11-20 00:03:54 -0600

    Updated fixture to current test env

--- ks-stage3-fixture.py
+++ ks-stage3-fixture.py
@@ -9,7 +9,19 @@ Interface.drop_collection()
 
 ## Populate with test data
 instance = Instance(hostname = "hornet01.mgmt.nw.com", tag = "apache2", status = "provisioned")
-interface = Interface(instance = instance, name = "eth0", mac_address = "08:00:27:b4:10:c6")
+interface = Interface(instance = instance, name = "eth0", mac_address = "08:00:27:1e:4b:7c")
+
+instance.save()
+interface.save()
+
+instance = Instance(hostname = "hornet02.mgmt.nw.com", tag = "apache2", status = "provisioned")
+interface = Interface(instance = instance, name = "eth0", mac_address = "08:00:27:f8:60:b3")
+
+instance.save()
+interface.save()
+
+instance = Instance(hostname = "hornet03.mgmt.nw.com", tag = "apache2", status = "provisioned")
+interface = Interface(instance = instance, name = "eth0", mac_address = "08:00:27:6c:2a:25")
 
 instance.save()
 interface.save()
